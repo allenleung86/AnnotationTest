edbded7644870da91e92e632dd2c54cad5c334c0
allenleung86 <londalonda@qq.com>
2017-03-15 16:18:04 +0800

    提交初始版本

---
+++ src/main/java/com/allen/FruitRun.java
@@ -0,0 +1,20 @@
+package com.allen;
+
+/**
+ * @title 输出结果
+ * @description
+ * @author liangzl
+ * @date 2017-03-15 16:02
+ */
+public class FruitRun {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        FruitInfoUtil.getFruitInfo(Apple.class);
+
+    }
+
+}
